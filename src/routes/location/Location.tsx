61590bb440ca19d762df1d3935f6c79ef75b6858
alwn8918 <alwn8918@gmail.com>
2025-09-01 16:58:03 +0900

    🔥 Remove: 빌드 오류 코드 삭제

--- src/routes/location/Location.tsx
+++ src/routes/location/Location.tsx
@@ -1,12 +1,6 @@
 import * as L from "./LocationStyle";
 import { Map, MapMarker, useKakaoLoader } from "react-kakao-maps-sdk";
-import React, {
-  useCallback,
-  useEffect,
-  useMemo,
-  useReducer,
-  useState,
-} from "react";
+import { useCallback, useEffect, useMemo, useReducer, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import Header from "@components/Header";
 import LocationModal from "@components/LocationModal";
