515dfcf7d4abc8c20332b0cb2d766e61ae02b659
alwn8918 <alwn8918@gmail.com>
2025-09-05 01:35:45 +0900

    ✨ Feat: 카카오 로그인 구현

--- src/routes/onboarding/Login.tsx
+++ src/routes/onboarding/Login.tsx
@@ -1,71 +1,27 @@
-import styled from "styled-components";
+// Login.tsx
 import Kakao from "@/assets/kakao_login.svg";
 import Background from "@assets/start_back.svg";
+import * as L from "./LoginStyle";
 
-const Container = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  background: white;
-  position: relative;
-
-  button {
-    position: absolute;
-    bottom: 58px;
-    padding: 0;
-  }
-`;
-
-const Back = styled.img`
-  position: absolute;
-  right: 0;
-  bottom: 0;
-`;
-
-const Title = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 14px;
-  position: absolute;
-  left: 38px;
-  top: 168px;
-
-  h1 {
-    color: ${({ theme }) => theme.colors.text1};
-    font-family: NanumSquareRound;
-    font-size: 36px;
-    font-weight: 800;
-    line-height: 125%;
-    white-space: pre-wrap;
-    margin: 0;
-  }
-
-  p {
-    color: ${({ theme }) => theme.colors.text2};
-    font-family: Pretendard;
-    font-size: 16px;
-    font-weight: 500;
-    line-height: 150%;
-    white-space: pre-wrap;
-    margin: 0;
-  }
-`;
+const API_URL = import.meta.env.VITE_API_URL;
+const LOGIN_URL = `${API_URL}/api/v1/auth/kakao/login`;
 
 const Login = () => {
+  const handleLogin = () => {
+    window.location.href = LOGIN_URL;
+  };
+
   return (
-    <Container>
-      <Back src={Background} alt="배경" />
-      <button>
+    <L.Container>
+      <L.Back src={Background} alt="배경" />
+      <button onClick={handleLogin}>
         <img src={Kakao} alt="카카오 로그인" />
       </button>
-      <Title>
+      <L.Title>
         <h1>{`재활용이\n쉬워지는\n똑똑한 한 컷`}</h1>
         <p>{`쓰레기 버리기 전, 딱 3초면 끝나는\n우리 동네 맞춤형 AI 분리수거 가이드`}</p>
-      </Title>
-    </Container>
+      </L.Title>
+    </L.Container>
   );
 };
 export default Login;
