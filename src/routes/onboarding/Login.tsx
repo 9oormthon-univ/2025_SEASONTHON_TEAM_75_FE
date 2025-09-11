f94be81ff5e6f91f5bee82006e743a67fc5edaaf
alwn8918 <alwn8918@gmail.com>
2025-09-11 18:54:36 +0900

    ✨ Feat: AuthStore 구현

--- src/routes/onboarding/Login.tsx
+++ src/routes/onboarding/Login.tsx
@@ -1,21 +1,22 @@
 import Kakao from "@/assets/kakao_login.svg";
 import Background from "@assets/start_back.svg";
 import * as L from "./LoginStyle";
-
-const API_URL = import.meta.env.VITE_API_URL;
-const LOGIN_URL = `${API_URL}/api/v1/auth/kakao/login`;
+import { useAuthActions } from "@stores/authStore";
 
 const Login = () => {
-  const handleLogin = () => {
-    window.location.href = LOGIN_URL;
-  };
+  const { loginWithKakao, loginAsGuest } = useAuthActions();
 
   return (
     <L.Container>
       <L.Back src={Background} alt="배경" />
-      <button onClick={handleLogin}>
-        <img src={Kakao} alt="카카오 로그인" />
-      </button>
+
+      <L.ButtonContainer>
+        <button onClick={loginWithKakao}>
+          <img src={Kakao} alt="카카오 로그인" />
+        </button>
+        <button onClick={loginAsGuest}>건너뛰기</button>
+      </L.ButtonContainer>
+
       <L.Title>
         <h1>{`재활용이\n쉬워지는\n똑똑한 한 컷`}</h1>
         <p>{`쓰레기 버리기 전, 딱 3초면 끝나는\n우리 동네 맞춤형 AI 분리수거 가이드`}</p>
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,4 @@ export * from "./home/schedule";
 export * from "./home/ranking";
 export * from "./home/revision";
 export * from "./location/location";
+export * from "./onboarding/user";
