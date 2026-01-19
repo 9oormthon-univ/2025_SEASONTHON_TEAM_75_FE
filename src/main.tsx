9885c2d08fae6810af1241a0050ace9d8462e39d
YYJ <yoon20220823@dukcung.ac.kr>
2026-01-19 20:43:09 +0900

    ✨ Feat: Sentry 모니터링 적용
    Sentry 모니터링 적용

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,23 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import "swiper/swiper-bundle.css";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+
+  tracesSampleRate: import.meta.env.DEV ? 1.0 : 0.1,
+  replaysOnErrorSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 );
